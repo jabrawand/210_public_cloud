8bfa713e550c7e54ca3e8f25831b2af277a5bc0a
jabrawand <jasmin_brawand@hotmail.com>
2026-06-20 06:14:33 +0200

    insert image in About and add content in Home

--- webpage-jabra/src/components/About.jsx
+++ webpage-jabra/src/components/About.jsx
@@ -1,47 +1,52 @@
 import React from 'react'
 import { athlete } from '../config/athlete'
+import { supabase } from '../supabase-client'
+import '../css/About.css'
 
 export default function About() {
+    const { data } = supabase.storage.from('images').getPublicUrl('/about/ap_ziel.JPG');
+    const publicUrl = data.publicUrl;
     return (
         <div className='about-container'>
-            <div className='about-section-image'>
+            <h1 className='about-section-title'>About {athlete.firstName}</h1>
 
-            </div>
-            <div className='about-section-content'>
-                <h1 className='about-section-title'>About {athlete.firstName}</h1>
+            <div className='about-body'>
+                <div className='about-section-image'>
+                    <img src={publicUrl} alt='About' />
+                </div>
 
-            </div>
-            <div className='about-section-info'>
-                <dl className='about-section-info-list'>
-                    <div className='about-page-row'>
-                        <dt className='about-page-label'>Name: </dt>
-                        <dd className='about-page-value'>{athlete.firstName} {athlete.lastName}</dd>
-                    </div>
-                    <div className='about-page-row'>
-                        <dt className='about-page-label'>Geburtstag: </dt>
-                        <dd className='about-page-value'>{athlete.birthdate}</dd>
-                    </div>
-                    <div className='about-page-row'>
-                        <dt className='about-page-label'>Wohnort: </dt>
-                        <dd className='about-page-value'>{athlete.location}</dd>
-                    </div>
-                    <div className='about-page-row'>
-                        <dt className='about-page-label'>Hobbys: </dt>
-                        <dd className='about-page-value'>{athlete.hobbies.join(', ')}</dd>
-                    </div>
-                    <div className='about-page-row'>
-                        <dt className='about-page-label'>Motivation: </dt>
-                        <dd className='about-page-value'>{athlete.motivation}</dd>
-                    </div>
-                    <div className='about-page-row'>
-                        <dt className='about-page-label'>Ziel: </dt>
-                        <dd className='about-page-value'>{athlete.goals}</dd>
-                    </div>
-                    <div className='about-page-row'>
-                        <dt className='about-page-label'>Sportliche Vergangenheit: </dt>
-                        <dd className='about-page-value'>{athlete.sportiveHistory}</dd>
-                    </div>
-                </dl>
+                <div className='about-section-info'>
+                    <dl className='about-section-info-list'>
+                        <div className='about-page-row'>
+                            <dt className='about-page-label'>Name: </dt>
+                            <dd className='about-page-value'>{athlete.firstName} {athlete.lastName}</dd>
+                        </div>
+                        <div className='about-page-row'>
+                            <dt className='about-page-label'>Geburtstag: </dt>
+                            <dd className='about-page-value'>{athlete.birthdate}</dd>
+                        </div>
+                        <div className='about-page-row'>
+                            <dt className='about-page-label'>Wohnort: </dt>
+                            <dd className='about-page-value'>{athlete.location}</dd>
+                        </div>
+                        <div className='about-page-row'>
+                            <dt className='about-page-label'>Hobbys: </dt>
+                            <dd className='about-page-value'>{athlete.hobbies.join(', ')}</dd>
+                        </div>
+                        <div className='about-page-row'>
+                            <dt className='about-page-label'>Motivation: </dt>
+                            <dd className='about-page-value'>{athlete.motivation}</dd>
+                        </div>
+                        <div className='about-page-row'>
+                            <dt className='about-page-label'>Ziel: </dt>
+                            <dd className='about-page-value'>{athlete.goals}</dd>
+                        </div>
+                        <div className='about-page-row'>
+                            <dt className='about-page-label'>Sportliche Vergangenheit: </dt>
+                            <dd className='about-page-value'>{athlete.sportiveHistory}</dd>
+                        </div>
+                    </dl>
+                </div>
             </div>
         </div>
     )
